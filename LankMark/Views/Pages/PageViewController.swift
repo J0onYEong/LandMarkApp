a5c502f96cf4a5f1a0c3c4909851835f5e83130c
choijun0 <cjy00050005@gmail.com>
2022-10-12 16:03:49 +0900

    +some caption

--- LankMark/Views/Pages/PageViewController.swift
+++ LankMark/Views/Pages/PageViewController.swift
@@ -12,9 +12,13 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
     @Binding var currentPage: Int
     
+    //SwiftUI calls this method before makeUIViewController(context:)
+    //so that you have access to the coordinator object when configuring your view controller.
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
+    
+    //SwiftUI calls this method a single time when it’s ready to display the view
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
@@ -26,19 +30,26 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        
+        //provide a view controller(UIHostingController) for display.
         pageViewController.setViewControllers(
             [context.coordinator.controllers[self.currentPage]], direction: .forward, animated: true)
     }
     
+    //-----------------------Cordinator Structure-----------------------
+    //A SwiftUI view that represents a UIKit view controller can define a Coordinator type
     class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         var controllers: [UIViewController]
 
         init(_ pageViewController: PageViewController) {
             self.parent = pageViewController
+            
+            //UIHostingController hosts the page SwiftUI view
             self.controllers = pageViewController.pages.map { UIHostingController(rootView: $0) }
         }
         
+        //-----------------------DataSource-----------------------
         func pageViewController(
             _ pageViewController: UIPageViewController,
             viewControllerBefore viewController: UIViewController) -> UIViewController?
@@ -51,7 +62,7 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             }
             return controllers[index - 1]
         }
-
+        
         func pageViewController(
             _ pageViewController: UIPageViewController,
             viewControllerAfter viewController: UIViewController) -> UIViewController?
@@ -64,6 +75,10 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             }
             return controllers[index + 1]
         }
+        //--------------------------------------------------------
+        
+        
+        //-----------------------Delegate-----------------------
         
         //SwiftUI calls this method whenever a page switching animation completes
         func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
